7e23cb341f1ac793770e8cdaa0bc4d89d1d2ae72
vhollo <vhollo@protonmail.com>
2022-08-14 15:55:51 +0200

    o

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,13 +4,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact';
-//export default /** @type {import('astro').AstroUserConfig} */ ({
+
 export default defineConfig({
   //   adapter: netlify(),
   output: 'static',
 
   /*buildOptions: {
-    site: 'https://cosyma.hu/',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     sitemap: true,      // Generate sitemap (set to "false" to disable)
   },*/
   site: 'https://cosyma.hu/',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
