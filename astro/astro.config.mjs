710987211ff41e4dc7d49ed8cb1b61e088fbad3d
vhollo <vhollo@protonmail.com>
2022-08-14 11:38:29 +0200

    astro update

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,26 +1,20 @@
-// export default {
-//   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-//   // pages: './src/pages',   // Path to Astro components, pages, and data
-//   // dist: './dist',       // When running `astro build`, path to final static output
-//   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-//   buildOptions: {
-//     site: 'https://astro-sanity-minimal-starter.netlify.app/',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-//     sitemap: true,      // Generate sitemap (set to "false" to disable)
-//   },
-//   devOptions: {
-//     // port: 3000,         // The port to run the dev server on.
-//     // tailwindConfig: '', // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
-//   },
-//   renderers: ['@astrojs/renderer-lit', '@astrojs/renderer-preact'],
-// };
+//import netlify from '@astrojs/netlify/functions';
 
 
 // @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-    buildOptions: {
-    site: 'https://astro-sanity-minimal-starter.netlify.app/',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+import { defineConfig } from 'astro/config'
+import preact from '@astrojs/preact';
+//export default /** @type {import('astro').AstroUserConfig} */ ({
+export default defineConfig({
+  //   adapter: netlify(),
+  output: 'static',
+
+  /*buildOptions: {
+    site: 'https://cosyma.hu/',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     sitemap: true,      // Generate sitemap (set to "false" to disable)
-  },
-  // Comment out "renderers: []" to enable Astro's default component support.
-  renderers: ['@astrojs/renderer-preact'],
+  },*/
+  site: 'https://cosyma.hu/',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  integrations: [
+    preact(),
+  ],
 });
