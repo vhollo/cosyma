2f08008b21d7de36b8f520c47cdb4417b9162862
vhollo <vhollo@protonmail.com>
2022-10-04 10:08:11 +0200

    ssr

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import svelte from "@astrojs/svelte";
 
-//import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.cosyma.hu/',
   integrations: [preact(), svelte()],
-  //adapter: netlify(),
-  output: 'static',
+  adapter: netlify(),
+  output: 'server',
 
   /*buildOptions: {
     sitemap: true,      // Generate sitemap (set to "false" to disable)
