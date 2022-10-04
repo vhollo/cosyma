c3b0a431723e4994d3f772c6064d5f3c1fc150f8
vhollo <vhollo@protonmail.com>
2022-10-04 10:06:33 +0200

    astro build

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,19 +1,17 @@
-//import netlify from '@astrojs/netlify/functions'; // @ts-check
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-
 import svelte from "@astrojs/svelte";
+
+//import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.cosyma.hu/',
+  integrations: [preact(), svelte()],
+  //adapter: netlify(),
   output: 'static',
-
-  /*adapter: netlify(),*/
 
   /*buildOptions: {
     sitemap: true,      // Generate sitemap (set to "false" to disable)
   },*/
-  site: 'https://www.cosyma.hu/',
-  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-  integrations: [preact(), svelte()]
 });
